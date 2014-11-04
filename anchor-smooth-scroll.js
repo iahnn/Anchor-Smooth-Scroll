0950a9331e1c764189c47eb465cb1cdc37f5a4f9
iahnn <iahnnlusica@gmail.com>
2014-11-04 14:46:23 +0800

    Update anchor-smooth-scroll.js

--- anchor-smooth-scroll.js
+++ anchor-smooth-scroll.js
@@ -10,23 +10,23 @@
 ;(function($) {
     $.fn.smoothScroll = function(options) {
         var opt = $.extend({
-            ease: "slow",
-            target: "target"
+            speed: "slow",
+            ease: "swing"
         }, options);
 
         var $body = $('html, body'),
             $this = $(this);
 
         $this.on('click', function(e) {
-            var dataId = $(this).data(opt.target),
+            var dataId = $(this).data("target"),
                 $target = $(dataId),
                 $sectionPos = $target.offset();
 
             if(typeof dataId !== "undefined" && $target.length > 0) {
-                $body.animate({scrollTop: $sectionPos.top}, opt.ease);
+                $body.animate({scrollTop: $sectionPos.top}, opt.speed, opt.ease);
                 e.preventDefault();
             }
-            
+
             return $(this);
         });
 
