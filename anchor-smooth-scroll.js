435c3357f8ee3068afda807422c3ccd90ba0c0a8
iahnn <iahnnlusica@gmail.com>
2014-11-04 14:18:00 +0800

    Update anchor-smooth-scroll.js

--- anchor-smooth-scroll.js
+++ anchor-smooth-scroll.js
@@ -1,34 +1,33 @@
+/**
+ * Anchor Smooth Scroll
+ * Requires jQuery Library
+ * Transform traditional anchor scroll into a smooth scroll effect
+ *
+ * @author Bryan Lusica
+ * @http://yourwebtech.info
+ **/
 
-	/**
-	 * Anchor Smooth Scroll
-	 * Requires jQuery 1.8 or higher
-	 * Transform traditional anchor scroll into a smooth scroll effect
-	 *
-	 * @author Bryan Lusica
-	 * @http://yourwebtech.info
-	 **/
-	 
-	;(function($) {
-				$.fn.smoothScroll = function(options) {
-					var opt = $.extend({
-						ease: "slow",
-						target: "target"
-					}, options);
+;(function($) {
+    $.fn.smoothScroll = function(options) {
+        var opt = $.extend({
+            ease: "slow",
+            target: "target"
+        }, options);
 
-					var $body = $('html, body'),
-						$this = $(this);
+        var $body = $('html, body'),
+            $this = $(this);
 
-					$this.on('click', function(e) {
-						var dataId = $(this).data(opt.target),
-							$target = $(dataId),
-							$sectionPos = $target.offset();
+        $this.on('click', function(e) {
+            var dataId = $(this).data(opt.target),
+                $target = $(dataId),
+                $sectionPos = $target.offset();
 
-						if(typeof dataId !== "undefined" && $target.length > 0) {
-							$body.animate({scrollTop: $sectionPos.top}, opt.ease);
-							e.preventDefault();
-						}
-					});
+            if(typeof dataId !== "undefined" && $target.length > 0) {
+                $body.animate({scrollTop: $sectionPos.top}, opt.ease);
+                e.preventDefault();
+            }
+        });
 
-					return $this;
-				};
-			})(jQuery);
+        return $this;
+    };
+})(jQuery);
